75b297364565700fb3a7d120a52b9be8e09481d0
Adryel <adryel.pnp@gmail.com>
2021-02-23 09:37:12 -0300

    feat: nextjs integration

--- src/components/ExperienceBar.tsx
+++ src/components/ExperienceBar.tsx
@@ -1,16 +1,14 @@
 import { useState } from "react"
 
 export function ExperienceBar() {
-  const [progress, setProgress] = useState(0)
-
   return (
     <header className="experience-bar">
       <span>0 xp</span>
       <div>
-        <div style={{ width: `${progress}%` }} />
+        <div style={{ width: "40%" }} />
 
-        <span className="current-experience" style={{ left: `${progress}%` }}>
-          {`${progress * 6} xp`}
+        <span className="current-experience" style={{ left: "40%" }}>
+          40 xp
         </span>
       </div>
       <span>600 xp</span>
